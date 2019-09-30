92dfd9a1cece1517c1f8072fe60054bac9229ba9
shahingazi <shahinkgc84@gmail.com>
2019-09-30 14:07:08 +0000

    adding validation

--- technical-training-13.0-01-models/openacademy/models/models.py
+++ technical-training-13.0-01-models/openacademy/models/models.py
@@ -1,4 +1,5 @@
 from odoo import api, models, fields
+from odoo.exceptions import ValidationError
 
 
 class Course(models.Model):
@@ -7,7 +8,7 @@ class Course(models.Model):
     
     name = fields.Char('Title')
     description = fields.Text('Description')
-    session_ids = fields.One2many('openacademy.session', 'course_id')
+    #session_ids = fields.One2many('openacademy.session', 'course_id')
 
 class Session(models.Model):
     _name = 'openacademy.session'
@@ -16,14 +17,31 @@ class Session(models.Model):
     name = fields.Char('Title')
     instructor_ids = fields.Many2many('openacademy.partner', string='Instructors', relation='session_partner_instructor_rel')
     course_id = fields.Many2one('openacademy.course', string='Course', required=True, ondelete='cascade')
-    attendee_ids = fields.Many2many('openacademy.partner', string='Attendees', relation='session_partner_student_rel')
+    attendee_ids = fields.Many2many('openacademy.partner', string='Attendees', relation='session_partner_student_rel')      
+    attendeetotal = fields.Float(compute='_compute_attendeetotal')
     
-    
+    @api.depends('attendee_ids')
+    def _compute_attendeetotal(self):       
+        for record in self:
+            record.attendeetotal = len(record.attendee_ids)
+            if record.attendeetotal > 2:
+                raise ValidationError('Maximum attendee exceed!')
+                
+                
 
 class Partner(models.Model):
     _name = 'openacademy.partner'
     _description = 'Partner'
     
-    name = fields.Char('Name')
+    name = fields.Char('First Name') 
+    lastname = fields.Char('Last Name')
+    fullname = fields.Char(compute = '_compute_fullname')
+    
     type = fields.Selection([('student','Student'), ('teacher', 'Teacher')], default='student')
+    
+    @api.depends('name', 'lastname')
+    def _compute_fullname(self):
+        for record in self:
+            record.fullname ="%s %s" %(record.name, record.lastname)
+            
     
